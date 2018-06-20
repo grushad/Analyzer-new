2074c7884efaf01de1921d095c2bf03d9457594f
grushad <40080803+grushad@users.noreply.github.com>
2018-06-20 10:13:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['loginbutton']))
+{
+session_start();
+$username=$_POST['admin-name'];
+$password=$_POST['admin-pass'];
+if($username=='admin' && $password=='12345')
+{
+$_SESSION['admin-name']=$username;
+header('Location:admin.html');
+}
+else{
+  require 'index.html';
+ $message = "Username and/or Password incorrect.\\nTry again.";
+  echo "<script type='text/javascript'>alert('$message');</script>";
+}
+}
+if(isset($_GET['logout'])){
+  session_start();
+  session_destroy();
+  require 'index.html';
+}
+?> 
